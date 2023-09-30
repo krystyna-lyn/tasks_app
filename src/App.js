3771f04a1a85f31deb80310b18da80cb4533ecbd
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-30 18:03:55 +0100

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+ const [task, setTask] = useState('');
+ const [tasks, setTasks] = useState([]);
+
+ const handleChange = (e) => {
+    setTask(e.target.value);
+ };
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation check: do not add todo if it's empty
+    if (task.trim() === '') {
+      alert('Please enter a task');
+      return;
+    }
+
+    setTasks([...tasks, { task, completed: false }]);
+    setTask('');
+ };
+
+ const toggleTaskCompleted = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+ };
+
+ return (
     <div>
-      <h1>Tasks</h1>
+      <h1>What to learn</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter task" value={task} onChange={handleChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input type='checkbox' checked={task.completed} onChange={() => toggleTaskCompleted(index)} />
+            {task.task}
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+ );
 }
 
 export default App;
